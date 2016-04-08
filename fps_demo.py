950978c10a9319f174adfc4b15c55ca2c8611329
Khaled Elkhawaga <k.elkhawaga@gmail.com>
2016-04-08 20:48:06 +0300

    Added files via upload
    initial commit

---
+++ fps_demo.py
@@ -0,0 +1,111 @@
+# USAGE
+# python fps_demo.py
+# python fps_demo.py --display 1
+
+# import the necessary packages
+from __future__ import print_function
+from imutils.video import WebcamVideoStream
+from imutils.video import FPS
+from time import sleep
+import serial
+import argparse
+import imutils
+import cv2
+import sys
+
+# new
+
+#ser = serial.Serial('/dev/ttyACM0', 9600)
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--num-frames", type=int, default=100,
+	help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=-1,
+	help="Whether or not frames should be displayed")
+args = vars(ap.parse_args())
+
+# grab a pointer to the video stream and initialize the FPS counter
+print("[INFO] sampling frames from webcam...")
+stream = cv2.VideoCapture(0)
+fps = FPS().start()
+
+# loop over some frames
+while fps._numFrames < args["num_frames"]:
+	# grab the frame from the stream and resize it to have a maximum
+	# width of 400 pixels
+	(grabbed, frame) = stream.read()
+	frame = imutils.resize(frame, width=400)
+
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.1,
+		minNeighbors=5,
+		minSize=(30, 30),
+		flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+	)
+
+	# Draw a rectangle around the faces
+	for (x, y, w, h) in faces:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+		send = (x*180)/400
+		print(x+w)
+		print(str(send))
+		#ser.write('p'+str(send)+'\n')
+		sleep(.1)
+
+
+
+	# check to see if the frame should be displayed to our screen
+	if args["display"] > 0:
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+	
+
+	# update the FPS counter
+	fps.update()
+	
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+        	break
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+stream.release()
+cv2.destroyAllWindows()
+
+# created a *threaded *video stream, allow the camera senor to warmup,
+# and start the FPS counter
+print("[INFO] sampling THREADED frames from webcam...")
+vs = WebcamVideoStream(src=0).start()
+fps = FPS().start()
+
+# loop over some frames...this time using the threaded stream
+while fps._numFrames < args["num_frames"]:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+
+	# check to see if the frame should be displayed to our screen
+	if args["display"] > 0:
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
