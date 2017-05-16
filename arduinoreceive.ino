a8f6ccb69a9b3816721241565d7775532c679bba
Khaled Elkhawaga <k.elkhawaga@gmail.com>
2017-05-16 19:49:36 +0200

    Filter input to prevent garbage

--- arduinoreceive.ino
+++ arduinoreceive.ino
@@ -2,6 +2,8 @@
 Servo servoTilt, servoPan;
 
 void setup() {
+  servoTilt.attach(2);
+  servoTilt.write(90);
   servoPan.attach(3);
   servoPan.write(90);
   Serial.begin(9600); // set the baud rate
@@ -10,10 +12,23 @@ void setup() {
 void loop() {
  while(Serial.available()) {
    char buffer[7];
-   Serial.readBytesUntil('n',buffer,7);
-   int incomingValue = atoi(buffer);
-   Serial.println(incomingValue);
-   servoPan.write(incomingValue);
+   char first = Serial.read();
+   if (first == 'p') {
+     Serial.readBytesUntil('\n',buffer,7);
+     int incomingValue = atoi(buffer);
+     if (incomingValue >= 0 && incomingValue <= 180) {
+       Serial.println(incomingValue);
+       servoPan.write(incomingValue);
+     }
+   }
+   if (first == 't') {
+     Serial.readBytesUntil('\n',buffer,7);
+     int incomingValue = atoi(buffer);
+     if (incomingValue >= 0 && incomingValue <= 180) {
+       Serial.println(incomingValue);
+       servoTilt.write(incomingValue);
+     }
+   }
    delay(100); // delay for 1/10 of a second
  }
 }
