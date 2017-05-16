ddbaebeab1cfc427b1adc75bc56a4124b3290ed0
Khaled Elkhawaga <k.elkhawaga@gmail.com>
2017-05-16 19:50:01 +0200

    Cleanup

--- facerec.py
+++ facerec.py
@@ -8,9 +8,12 @@ fn_haar = 'haarcascade_frontalface_default.xml'
 fn_dir = 'att_faces'
 fn_name = sys.argv[1]
 
-# Part 1: Create fisherRecognizer
+# Create fisherRecognizer
+
 print('Training...')
+
 # Create a list of images and a list of corresponding names
+
 (images, labels, names, id) = ([], [], {}, 0)
 for (subdirs, dirs, files) in os.walk(fn_dir):
     for subdir in dirs:
@@ -25,26 +28,29 @@ for (subdirs, dirs, files) in os.walk(fn_dir):
 (im_width, im_height) = (112, 92)
 
 # Create a Numpy array from the two lists above
+
 (images, labels) = [numpy.array(lis) for lis in [images, labels]]
 
 # OpenCV trains a model from the images
 # NOTE FOR OpenCV2: remove '.face'
+
 model = cv2.createFisherFaceRecognizer()
 model.train(images, labels)
 
-# Part 2: Use fisherRecognizer on camera stream
+# Use fisherRecognizer on camera stream
+
 haar_cascade = cv2.CascadeClassifier(fn_haar)
 webcam = cv2.VideoCapture(1)
 
-# added
+# Initialize servo positions
 
 servoTiltPos = 90
 servoPanPos = 90
 servoStep = 1
 
-ser = serial.Serial('/dev/ttyACM0', 57600)
+# Initialize serial connection
 
-# end added
+ser = serial.Serial('/dev/ttyACM0', 57600)
 
 while True:
     (rval, frame) = webcam.read()
@@ -52,14 +58,12 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     mini = cv2.resize(gray, (gray.shape[1] / size, gray.shape[0] / size))
 
-    # added
+    # Initialize midpoint and tolerance values
 
     midScreenX = (gray.shape[1] / size) / 2
     midScreenY = (gray.shape[0] / size) / 2
     tolX = 0.1 * (gray.shape[1] / size)
     tolY = 0.1 * (gray.shape[0] / size)
-
-    # end added
 
     faces = haar_cascade.detectMultiScale(mini)
     for i in range(len(faces)):
@@ -69,43 +73,32 @@ while True:
         face_resize = cv2.resize(face, (im_width, im_height))
 
         # Try to recognize the face
+        
         prediction = model.predict(face_resize)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
-        # Write the name of recognized face
-        # [1]
-        # attempt unknown
+        # If found, write the name of recognized face
 
         if prediction[1]<1500:
             cv2.putText(frame,
             '%s - %.0f' % (names[prediction[0]],prediction[1]),
             (x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
+
+        # Otherwise, write 'Unknown'
+        
         else:
             cv2.putText(frame,
             'Unknown',
             (x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
 
-        # end attempt unknown
-        # cv2.putText(frame,
-        #     '%s - %.0f' % (names[prediction[0]],prediction[1]),
-        #     (x-10, y-10), cv2.FONT_HERSHEY_PLAIN,1,(0, 255, 0))
-
         if (names[prediction[0]] == fn_name):
 
-            # added, servo
 
             midFaceX = face_i[0] + (face_i[2] / 2)
             midFaceY = face_i[1] + (face_i[3] / 2)
 
-            # print("midFaceX = " + str(midFaceX))
-            # print("midScreenX = " + str(midScreenX))
-            # print("midScreenX - tol = " + str(midScreenX - tolX))
-            #
-            # print("midFaceY = " + str(midFaceY))
-            # print("midScreenY = " + str(midScreenY))
-            # print("midScreenY - tol = " + str(midScreenY - tolY))
-
-            # Check face position in relation to screen mid
+            # Check face position in relation to the midpoint
+            
             if midFaceY < midScreenY - tolY:
                 if servoTiltPos >= 5:
                     servoTiltPos -= servoStep
@@ -119,15 +112,14 @@ while True:
                 if servoPanPos <= 175:
                     servoPanPos += servoStep
 
-            print('Tilt pos' + str(servoTiltPos))
-            print('Pan pos' + str(servoPanPos))
+            # Send pan and tilt positions to servos over serial
+
             ser.write('t')
             ser.write(chr(servoTiltPos))
             ser.write('p')
             ser.write(chr(servoPanPos))
             sleep(0.05)
 
-            # end, servo
 
     cv2.imshow('OpenCV', frame)
     key = cv2.waitKey(10)
